7f6815b7670fbdd9911101326662d31d0dc7825c
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-20 06:17:28 -0400

    Add basic page viewing capability

--- onepage/pages/views.py
+++ onepage/pages/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from .models import Page
 
 def view_page(request, username):
-    """"""
-    return render(request, "pages/view_page.html")
+    """Renders the profile page for the given user."""
+    page_data = Page.objects.filter(user__username=username).first()
+    print(page_data)
+    if not page_data:
+        return HttpResponseNotFound()
+    return render(request, "pages/view_page.html", {
+        "page_data": page_data,
+    })
