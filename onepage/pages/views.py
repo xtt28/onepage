fb64a4431a963b071636e859dc91eabc93cddf8e
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-21 06:20:58 -0400

    Add form to edit user profile page

--- onepage/pages/views.py
+++ onepage/pages/views.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django import forms
 from .models import Page
+
+
+class PageForm(forms.ModelForm):
+    """A form that allows the user to update settings about their page."""
+
+    class Meta:
+        model = Page
+        fields = ["description"]
 
 
 def view_page(request, username):
@@ -17,5 +27,16 @@ def view_page(request, username):
 
 @login_required
 def edit_page(request):
-    """Renders a view that allows a user to modify their profile page."""
-    return render(request, "pages/edit_page.html")
+    """If the request method is GET, renders a view that allows a user to modify
+    their profile page. If the method is POST, accepts a PageForm to update the
+    user's profile page."""
+    page = Page.objects.get(user=request.user)
+
+    if request.method == "POST":
+        form = PageForm(request.POST, instance=page)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f"/@{request.user.username}")
+    else:
+        form = PageForm(instance=page)
+    return render(request, "pages/edit_page.html", {"form": form})
