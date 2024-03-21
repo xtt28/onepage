608d91ad6f457e65f58046e08129ebce02ce3af0
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-21 15:53:29 -0400

    Add names to URLs

--- onepage/pages/urls.py
+++ onepage/pages/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from .views import edit_page, view_page
 
-urlpatterns = [path("@<username>", view_page), path("edit", edit_page)]
+urlpatterns = [
+    path("@<username>", view_page, name="view_page"),
+    path("edit", edit_page, name="edit_page"),
+]
--- onepage/users/urls.py
+++ onepage/users/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .views import dashboard
 
-urlpatterns = [path("you", dashboard)]
+urlpatterns = [path("you", dashboard, name="dashboard")]
