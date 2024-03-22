73c0ceb9760c9e9669fb18c1af07eb940cf53cc0
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-22 05:55:23 -0400

    Remove dashboard view and template

--- onepage/users/views.py
+++ onepage/users/views.py
@@ -1,9 +1,2 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
-
-@login_required
-def dashboard(request):
-    """Renders a dashboard with links for authenticated users to manage their
-    profiles."""
-    return render(request, "users/dashboard.html")
