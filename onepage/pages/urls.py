7ccdcf076969306910f7a1d2b12433cc9bc927f4
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-23 07:05:56 -0400

    Add profile page link creation functionality

--- onepage/pages/urls.py
+++ onepage/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import edit_page, view_page
+from .views import edit_page, view_page, create_link
 
 urlpatterns = [
     path("@<username>", view_page, name="view_page"),
     path("edit", edit_page, name="edit_page"),
+    path("edit/links", create_link, name="create_link"),
 ]
--- onepage/pages/views.py
+++ onepage/pages/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotAllowed, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django import forms
-from .models import Page
+from .models import Page, PageLink
 
 
 class PageForm(forms.ModelForm):
@@ -11,6 +11,14 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         fields = ["description"]
+
+
+class PageLinkForm(forms.ModelForm):
+    """A form that allows the user to update or create a link for their page."""
+
+    class Meta:
+        model = PageLink
+        fields = ["url"]
 
 
 def view_page(request, username):
@@ -39,4 +47,25 @@ def edit_page(request):
             return HttpResponseRedirect(f"/@{request.user.username}")
     else:
         form = PageForm(instance=page)
-    return render(request, "pages/edit_page.html", {"form": form})
+        link_form = PageLinkForm()
+    return render(
+        request, "pages/edit_page.html", {"form": form, "link_form": link_form}
+    )
+
+
+@login_required
+def create_link(request):
+    """Creates a link for the user's profile page with the data from the POST
+    request."""
+    if request.method != "POST":
+        return HttpResponseNotAllowed(permitted_methods=["POST"])
+
+    user_page = Page.objects.get(user=request.user)
+    new_link = PageLink(page_id=user_page.pk)
+
+    form = PageLinkForm(request.POST, instance=new_link)
+    if form.is_valid():
+        form.save()
+        return HttpResponse("Link added")
+    else:
+        return HttpResponse(form.errors.as_text())
