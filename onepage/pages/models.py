e5a3d3e1a77b4741351c928895cfbd9025c37caf
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-24 07:27:22 -0400

    Use profile usernames instead of links for recognized social platforms

--- onepage/pages/models.py
+++ onepage/pages/models.py
@@ -1,3 +1,7 @@
+from urllib.parse import quote_plus
+
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from users.models import AppUser
@@ -26,9 +30,8 @@ class PageLink(models.Model):
         FACEBOOK = "facebook", _("Facebook")
         INSTAGRAM = "instagram", _("Instagram")
         LINKEDIN = "linkedin", _("LinkedIn")
-        SPOTIFY = "spotify", _("Spotify")
-        APPLE_MUSIC = "apple_music", _("Apple Music")
-        DISCORD = "discord", _("Discord")
+        SPOTIFY = "spotify", _("Spotify Artist ID")
+        DISCORD = "discord", _("Discord Invite ID")
         TELEGRAM = "telegram", _("Telegram")
         WHATSAPP = "whatsapp", _("WhatsApp")
         TUMBLR = "tumblr", _("Tumblr")
@@ -44,7 +47,37 @@ class PageLink(models.Model):
         choices=Platform.choices,
         default=Platform.OTHER,
     )
-    url = models.URLField()
+    value = models.TextField()
+
+    def get_profile_link(self):
+        safe_value = quote_plus(self.value)
+        match self.platform:
+            case "youtube" | "tiktok" | "x" | "tumblr":
+                return f"https://{self.platform}.com/@{safe_value}"
+            case "facebook" | "instagram" | "github":
+                return f"https://{self.platform}.com/{safe_value}"
+            case "spotify":
+                return f"https://open.spotify.com/artist/{safe_value}"
+            case "discord":
+                return f"https://discord.com/invite/{safe_value}"
+            case "telegram":
+                return f"https://t.me/{safe_value}"
+            case "whatsapp":
+                return f"https://chat.whatsapp.com/{safe_value}"
+            case "reddit":
+                return f"https://reddit.com/u/{safe_value}"
+            case "cash_app":
+                return f"https://cash.app/{safe_value}"
+            case "paypal":
+                return f"https://paypal.me/{safe_value}"
+            case "other" | _:
+                validate_url = URLValidator()
+
+                try:
+                    validate_url(self.value)
+                    return self.value
+                except ValidationError:
+                    return ""
 
     def __str__(self):
-        return f"Link to {self.url}"
+        return f"{self.platform} link: {self.value}"
