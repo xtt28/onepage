bafbb170367df0d10fe229bc8d25d5b6b241013d
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-26 18:37:23 -0400

    Add test cases for user logout

--- onepage/users/tests.py
+++ onepage/users/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user
 from django.test import TestCase
 from users.models import AppUser
 from pages.models import Page, PageLink
@@ -29,3 +30,26 @@ class UserDeleteTestCase(TestCase):
         response = self.client.get(self.PATH)
         self.assertEqual(AppUser.objects.count(), 0)
         self.assertEqual(Page.objects.count(), 0)
+
+
+class UserLogoutTestCase(TestCase):
+    PATH = "/you/logout"
+
+    def setUp(self):
+        test_user = AppUser.objects.create(username="test_user")
+        test_user.set_password("test_pass")
+        test_user.save()
+
+    def test_handles_unauthenticated_user(self):
+        """A request to log out by an unauthenticated user should not succed."""
+        response = self.client.get(self.PATH)
+        self.assertNotEqual(200, response.status_code)
+
+    def test_logs_out(self):
+        """A request to log out by an authenticated user should log the user
+        out."""
+        self.client.login(username="test_user", password="test_pass")
+        self.client.get(self.PATH)
+
+        user = get_user(self.client)
+        self.assertFalse(user.is_authenticated)
