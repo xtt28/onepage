dc1bcf3d704ad7766e78fefc6c2805db07952e33
xtt28 <155261788+xtt28@users.noreply.github.com>
2024-03-26 16:47:13 -0400

    Require login for user settings page

--- onepage/users/views.py
+++ onepage/users/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_GET
 from django.shortcuts import render
 
 
+@login_required
 @require_GET
 def user_settings(request):
     return render(request, "users/settings.html")
